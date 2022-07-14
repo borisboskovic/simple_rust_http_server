751f0e0e53725877133e66b0b94fdb9e98a9fd7a
Boris Bošković <boris.boskovic92@gmail.com>
2022-07-14 21:49:10 +0200

    Added Response struct implementation

---
+++ src/http/status_code.rs
@@ -0,0 +1,25 @@
+use std::fmt::{Display, Formatter, Result as FmtResult};
+
+#[derive(Clone, Copy, Debug)]
+pub enum StatusCode {
+    Ok = 200,
+    BadRequest = 400,
+    NotFound = 404,
+}
+
+impl StatusCode {
+    pub fn reason_phrase(&self) -> &str {
+        match self {
+            Self::Ok => "OK",
+            Self::BadRequest => "BAD REQUEST",
+            Self::NotFound => "NOT FOUND",
+        }
+    }
+}
+
+impl Display for StatusCode {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(f, "{}", *self as u16);
+        todo!()
+    }
+}
