e95f657dfc89b18c2ac346ddcfdc4a2011f136c3
Boris Bošković <boris.boskovic92@gmail.com>
2022-07-17 23:52:07 +0200

    Serving static files

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,5 +1,6 @@
 use super::server::Handler;
 use crate::http::{Method, Request, Response, StatusCode};
+use std::{fmt::format, fs};
 
 pub struct WebsiteHandler {
     public_path: String,
@@ -9,15 +10,35 @@ impl WebsiteHandler {
     pub fn new(public_path: String) -> Self {
         Self { public_path }
     }
+
+    fn read_file(&self, file_path: &str) -> Option<String> {
+        let path = format!("{}\\{}", self.public_path, file_path);
+
+        match fs::canonicalize(path) {
+            Ok(path) => {
+                if path.starts_with(&self.public_path) {
+                    return fs::read_to_string(path).ok();
+                } else {
+                    println!("Directory traversal attack attempted! Path: {}", file_path);
+                    return None;
+                }
+            }
+            Err(_) => return None,
+        }
+    }
 }
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
         match request.method() {
             Method::GET => match request.path() {
-                "/" => Response::new(StatusCode::Ok, Some("<h1>Home page</h1>".to_string())),
-                "/hello" => Response::new(StatusCode::Ok, Some("<h1>Hello world</h1>".to_string())),
-                _ => Response::new(StatusCode::NotFound, None),
+                "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
+                "/hello" => Response::new(StatusCode::Ok, self.read_file("hello.html")),
+                // _ => Response::new(StatusCode::NotFound, None),
+                path => match self.read_file(path) {
+                    Some(content) => Response::new(StatusCode::Ok, Some(content)),
+                    None => Response::new(StatusCode::NotFound, None),
+                },
             },
             _ => Response::new(StatusCode::NotFound, None),
         }
