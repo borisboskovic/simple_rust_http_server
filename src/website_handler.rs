c152216cec8b3ada2938e8a2e35bf2cbd6ead051
Boris Bošković <boris.boskovic92@gmail.com>
2022-07-17 22:54:07 +0200

    Sending different responses based on path and method

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,10 +1,17 @@
 use super::server::Handler;
-use crate::http::{Request, Response, StatusCode};
+use crate::http::{Method, Request, Response, StatusCode};
 
 pub struct WebsiteHandler;
 
 impl Handler for WebsiteHandler {
-    fn handle_request(&mut self, request: &crate::http::Request) -> Response {
-        Response::new(StatusCode::Ok, Some("<h1>Rust Server</h1>".to_string()))
+    fn handle_request(&mut self, request: &Request) -> Response {
+        match request.method() {
+            Method::GET => match request.path() {
+                "/" => Response::new(StatusCode::Ok, Some("<h1>Home page</h1>".to_string())),
+                "/hello" => Response::new(StatusCode::Ok, Some("<h1>Hello world</h1>".to_string())),
+                _ => Response::new(StatusCode::NotFound, None),
+            },
+            _ => Response::new(StatusCode::NotFound, None),
+        }
     }
 }
